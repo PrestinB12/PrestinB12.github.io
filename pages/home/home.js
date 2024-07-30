d82fc14873f4d3e35e0e46423844fefbe3ce4b04
Prestin Bell <prestin97@gmail.com>
2024-07-30 18:53:49 -0400

    more fixes

--- pages/home/home.js
+++
@@ -1,9 +0,0 @@
-function showContent() {
-    // Get the content inside the template and append it to the body
-    const template = document.querySelector('template');
-    const clone = document.importNode(template.content, true);
-    document.body.appendChild(clone);
-    
-    // Optionally, remove the template to prevent duplicate content
-    template.remove();
-}
