6a527eea4f06f3212e1a89f24ef62f80513fc009
Prestin Bell <prestin97@gmail.com>
2024-07-30 17:55:52 -0400

    future dark mode toggle save-state function

---
+++ app.js
@@ -0,0 +1,7 @@
+
+/*
+    For saving state for future dark-mode on website toggle (coming soon)
+window.localStorage.setItem('settings.darkmode', "on")
+...
+const darkMode = ('on' === window.localStorage.getItem('settings.darkmode'));
+*/
